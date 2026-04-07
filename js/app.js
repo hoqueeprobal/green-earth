8025e08bb05d377eacf29ba8bbdfce146dafdb8f
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2026-04-07 17:19:10 +0600

    Initial setup

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// DOM references and global state
+const categoryContainer = document.getElementById("category-container");
+const postContainer = document.getElementById("post-container");
+const loader = document.getElementById("loader");
+const modalContainer = document.getElementById("modal-container");
+const cartContainer = document.getElementById("cart-container");
+const totalPrice = document.getElementById("total-price");
+
+// Store cart items
+let addToCart = [];
